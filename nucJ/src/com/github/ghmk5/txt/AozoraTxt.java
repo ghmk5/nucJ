4d05651fde81e7a2994695a6b2815f79c30733e6
mk5 <mramatz@remus.dti.ne.jp>
2018-12-10 00:55:00 +0900

    AozoraTxtクラスに分割・整形機能を実装

--- nucJ/src/com/github/ghmk5/txt/AozoraTxt.java
+++ nucJ/src/com/github/ghmk5/txt/AozoraTxt.java
@@ -1,10 +1,14 @@
 package com.github.ghmk5.txt;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,6 +22,7 @@ public class AozoraTxt {
 
   String title;
   String author;
+  String summary;
   ArrayList<AozoraChapter> listOfChapters;
   // ArrayList<AozoraSection> listOfSections;
 
@@ -25,10 +30,12 @@ public class AozoraTxt {
   ArrayList<String> titlePage;
 
   /**
+   * 青空文庫テキストの本文およびタイトル・著者名・章・節などのメタ情報を含んだオブジェクト
+   * 
    * @param aozoraTxtFile
    *          WebAozoraConverterが生成したconverted.txt
    * @param charCode
-   *          InputStreamReaderに与える文字コード判別子
+   *          InputStreamReaderに与える文字コード識別子
    * @throws IOException
    */
   public AozoraTxt(File aozoraTxtFile, String charCode) throws IOException {
@@ -37,26 +44,34 @@ public class AozoraTxt {
 
     this.bufferedReader = new BufferedReader(inputStreamReader);
 
-    titlePage = new ArrayList<>();
-
-    // 最初の［＃改ページ］までをタイトルページとして読み出す
+    // 第一行からタイトル、第二行から著者名を取得
     this.title = bufferedReader.readLine();
-    titlePage.add(this.title);
     this.title = StringUtils.chomp(this.title);
     this.author = bufferedReader.readLine();
-    titlePage.add(this.author);
     this.author = StringUtils.chomp(this.author);
 
+    // 最初の改ページまでの部分から概要を取得
     Pattern patternPageBreak = Pattern.compile("^［＃改ページ］$");
-    String line;
+    Matcher matcherPageBreak;
+    Pattern patternAozoraTag = Pattern.compile("^［＃");
+    Matcher matcherAozoraTag;
     boolean flgBreakRead = false;
+    this.summary = "";
     while (!flgBreakRead) {
-      line = bufferedReader.readLine();
-      Matcher matcherPageBreak = patternPageBreak.matcher(line);
-      titlePage.add(line);
+      String line = bufferedReader.readLine();
+      matcherPageBreak = patternPageBreak.matcher(line);
+      matcherAozoraTag = patternAozoraTag.matcher(line);
+      if (!matcherAozoraTag.find()) {
+        if (!line.equals("")) {
+          summary += line;
+          summary += "\n";
+        }
+      }
       if (matcherPageBreak.find())
         flgBreakRead = true;
     }
+    this.summary = this.summary.replaceAll("^\n+", "");
+    this.summary = this.summary.replaceAll("\n+$", "");
 
     listOfChapters = new ArrayList<>();
 
@@ -70,7 +85,7 @@ public class AozoraTxt {
     Matcher matcherSectionTagEnds;
     Pattern pDateTagStarts = Pattern.compile("^［＃ここから地から１字上げ］$");
     Matcher matcherDateStarts;
-    Pattern pDateTagEnds = Pattern.compile("^［＃ここで字上げ終わり］$");
+    Pattern pDateTagEnds = Pattern.compile("^［＃ここで字上げ終わり］$"); // このタグは挿絵・囲み記述の後ろにも出てくるが、既にfalseになっているフラグを更にfalseにするだけなので問題ない
     Matcher matcherDateEnds;
     Pattern pDatePublished = Pattern.compile("([0-9]{4}/[0-9]{2}/[0-9]{2} [0-9]{2}:[0-9]{2}) +公開");
     Matcher matcherDatePublished;
@@ -91,6 +106,7 @@ public class AozoraTxt {
     String sectionTitle;
     AozoraSection currentSection = new AozoraSection(sectionIdx, chapterIdx, "defaultSectionTitle");
 
+    String line;
     while ((line = bufferedReader.readLine()) != null) {
       matcherChapterTagStarts = pChapterTagStarts.matcher(line);
       matcherChapterTagEnds = pChapterTagEnds.matcher(line);
@@ -133,7 +149,7 @@ public class AozoraTxt {
           sectionTitle = StringUtils.chomp(line);
           currentSection = new AozoraSection(sectionIdx, chapterIdx, sectionTitle);
         }
-      } else if (flgDateTag) {
+      } else if (flgDateTag) { // タグ行を含め、投稿改稿日時部分を読んでいる
         if (!matcharTagLine.find()) { // タグ行ではない = 投稿改稿日時の行
           if (matcherDatePublished.find())
             currentSection.setDatePublished(matcherDatePublished.group(1));
@@ -160,18 +176,247 @@ public class AozoraTxt {
     bufferedReader.close();
   }
 
+  /**
+   * タイトルを返す
+   * 
+   * @return String title
+   */
   public String getTitle() {
     return this.title;
   }
 
+  /**
+   * 著者名を返す
+   * 
+   * @return String author
+   */
   public String getAuthor() {
     return this.author;
   }
 
+  /**
+   * 章のリストを返す
+   * 
+   * @return ArrayList<AozoraChapter> listOfChapters
+   */
   public ArrayList<AozoraChapter> getListOfChapters() {
     return this.listOfChapters;
   }
 
+  /**
+   * 分割した青空文庫テキストファイルを保存し、EPUB3変換用ファイルのリストを返す
+   * 
+   * @param dstPathForViewer
+   *          ビューワー閲覧用ファイルの保存パス
+   * @param dstPathForEPUB3
+   *          EPUB3変換用ファイルの保存パス
+   * @param volumeLength
+   *          分割ファイルの文字数 この数字を超えないように節単位で分割される
+   * @param forceChapterwise
+   *          この値がtrueのとき、強制的に章単位で分割する
+   * @param flagOutputForViewer
+   *          この値がtrueのとき、ビューワー閲覧用ファイルを出力する
+   * @param flagOutputForEPUB3
+   *          この値がtrueのとき、EPUB3変換用ファイルを出力する
+   * @param allowSingleEmptyLine
+   *          この値がtrueのとき、本文中の単一の空行をそのまま残す パラグラフ毎に空行を挟んでいる作品用
+   *          successiveEmptyLinesLimitと合わせて用いることにより、単一の空行は削除し、連続した空行を一つにまとめることができる
+   * @param successiveEmptyLinesLimit
+   *          連続した空行の最大数 この数を超えて連続する空行はこの数まで減らされる
+   * @return EPUB3変換用ファイルのリスト
+   * @throws IOException
+   */
+  public ArrayList<File> split(String dstPathForViewer, String dstPathForEPUB3, int volumeLength,
+      boolean forceChapterwise, boolean flagOutputForViewer, boolean flagOutputForEPUB3, boolean allowSingleEmptyLine,
+      int successiveEmptyLinesLimit) throws IOException {
+    ArrayList<File> outFiles = new ArrayList<>();
+
+    String fileNameForViewer;
+    File dstFileForViewer;
+    FileOutputStream fosForViewer;
+    Writer writerForViewer;
+    BufferedWriter bwForViewer;
+
+    String fileNameForEPUB3;
+    File dstFileForEPUB3;
+    FileOutputStream fosForEPUB3;
+    Writer writerForEPUB3;
+    BufferedWriter bwForEPUB3;
+
+    int volumeNumber = 1;
+
+    // 「初期化されていない可能性がある」エラーを回避するのが難しいので、直接BufferedWriterに流し込むのではなく、
+    // 一旦ArrayList<String>に流し込んでから、出力フラグに応じて書き出すようにする
+    // -- BufferedWriterを初期化するとファイルが生成されるので、フラグがfalseなら出力しないという動作を実現する上で都合が悪い
+    ArrayList<ArrayList<String>> volumesAsListForEPUB3 = new ArrayList<>();
+    ArrayList<String> contentsAsListForEPUB3 = new ArrayList<>();
+    ArrayList<ArrayList<String>> volumesAsListForViewer = new ArrayList<>();
+    ArrayList<String> contentsAsListForViewer = new ArrayList<>();
+
+    contentsAsListForEPUB3.add(this.titlePageForEPUB3(volumeNumber));
+    contentsAsListForViewer.add(this.titlePageForViewer(volumeNumber));
+
+    // ビューワー用出力に含めないタグ行を検出するためのパターンとマッチャ
+    // Pattern pTagSectionLine = Pattern.compile("［＃区切り線］");
+    // Matcher mTagSectionLine;
+    Pattern pTagMinusIndentStart = Pattern.compile("［＃ここから.字上げ］");
+    Matcher mTagMinusIndentStart;
+    Pattern pTagMinusIndentEnd = Pattern.compile("［＃ここで字上げ終わり］");
+    Matcher mTagMinusIndentEnd;
+    Pattern pTagSetSmallStart = Pattern.compile("［＃ここから.段階小さな文字］");
+    Matcher mTagSetSmallStart;
+    Pattern pTagSetSmallEnd = Pattern.compile("［＃ここで小さな文字終わり］");
+    Matcher mTagSetSmallEnd;
+
+    int currentLength = 0;
+    String dateLine = "";
+
+    for (AozoraChapter aozoraChapter : this.getListOfChapters()) {
+
+      if (aozoraChapter.length == 0) {
+        continue; // 章分けがある場合のデフォルト章 空のはず
+      } else if (!aozoraChapter.title.equals("defaultChapterTitle")) { // 章分けありの通常章なら章タイトルを書き込む
+        contentsAsListForEPUB3.add("［＃ここから大見出し］\n" + aozoraChapter.title + "\n［＃ここで大見出し終わり］\n\n");
+        contentsAsListForViewer
+            .add("［＃ここから３字下げ］\n［＃ここから大見出し］\n" + aozoraChapter.title + "\n［＃ここで大見出し終わり］\n［＃ここで字下げ終わり］\n\n");
+      } // 章分けなしの場合、章タイトルの書き込みをスキップする
+
+      for (AozoraSection aozoraSection : aozoraChapter.getListOfSections()) {
+        if (currentLength + aozoraSection.length > volumeLength) {
+          //  規定容量に到達したvolumeをリストに追加・volumeインクリメント・新規volumeを生成
+          volumesAsListForEPUB3.add(contentsAsListForEPUB3);
+          contentsAsListForEPUB3 = new ArrayList<>();
+          volumesAsListForViewer.add(contentsAsListForViewer);
+          contentsAsListForViewer = new ArrayList<>();
+          currentLength = 0;
+          volumeNumber++;
+          // 新規volumeの先頭にタイトルページを追加
+          contentsAsListForEPUB3.add(this.titlePageForEPUB3(volumeNumber));
+          contentsAsListForViewer.add(this.titlePageForViewer(volumeNumber));
+        }
+        currentLength += aozoraSection.length;
+
+        // 節タイトルを追加
+        contentsAsListForEPUB3.add("［＃ここから中見出し］\n" + aozoraSection.title + "\n［＃ここで中見出し終わり］\n\n");
+        contentsAsListForViewer
+            .add("\n［＃ここから５字下げ］\n［＃ここから中見出し］\n" + aozoraSection.title + "\n［＃ここで中見出し終わり］\n［＃ここで字下げ終わり］\n\n");
+
+        // 投稿改稿日時を追加
+        dateLine = "［＃ここから地から１字上げ］\n［＃ここから１段階小さな文字］\n" + aozoraSection.datePublished + " 公開";
+        if (aozoraSection.getDateUpdated() != null)
+          dateLine += "　" + aozoraSection.getDateUpdated() + " 改稿";
+        dateLine += "\n［＃ここで小さな文字終わり］\n［＃ここで字上げ終わり］\n";
+        contentsAsListForEPUB3.add(dateLine);
+
+        dateLine = "［＃ここから地から１字上げ］\n" + aozoraSection.datePublished + " 公開";
+        if (aozoraSection.getDateUpdated() != null)
+          dateLine += "　" + aozoraSection.getDateUpdated() + " 改稿";
+        dateLine += "\n［＃ここで字上げ終わり］\n\n";
+        contentsAsListForViewer.add(dateLine);
+
+        // 本文を追加
+        for (String line : aozoraSection.getEmptyLinesTreatedBody(allowSingleEmptyLine, successiveEmptyLinesLimit)) {
+          contentsAsListForEPUB3.add(line + "\n");
+
+          // ビューワーに解釈できないタグの行を除く処理 字上げ 文字サイズ
+          // TODO 区切り線はあとで字下げタグと一緒に置換したほうがきれいな仕上がりになると思う
+          mTagMinusIndentStart = pTagMinusIndentStart.matcher(line);
+          mTagMinusIndentEnd = pTagMinusIndentEnd.matcher(line);
+          mTagSetSmallStart = pTagSetSmallStart.matcher(line);
+          mTagSetSmallEnd = pTagSetSmallEnd.matcher(line);
+          if (!mTagMinusIndentStart.find() && !mTagMinusIndentEnd.find() && !mTagSetSmallStart.find()
+              && !mTagSetSmallEnd.find())
+            contentsAsListForViewer.add(line + "\n");
+        }
+      }
+    }
+    volumesAsListForEPUB3.add(contentsAsListForEPUB3);
+    volumesAsListForViewer.add(contentsAsListForViewer);
+
+    volumeNumber = 1;
+    if (flagOutputForEPUB3) {
+      for (ArrayList<String> contentsAsList : volumesAsListForEPUB3) {
+        fileNameForEPUB3 = "converted" + String.format("%02d", volumeNumber) + ".txt";
+        if (!dstPathForEPUB3.endsWith("/"))
+          dstPathForEPUB3 += "/";
+        dstFileForEPUB3 = new File(dstPathForEPUB3 + fileNameForEPUB3);
+        fosForEPUB3 = new FileOutputStream(dstFileForEPUB3);
+        writerForEPUB3 = new OutputStreamWriter(fosForEPUB3, "UTF-8");
+        bwForEPUB3 = new BufferedWriter(writerForEPUB3);
+        for (String line : contentsAsList) {
+          bwForEPUB3.write(line);
+        }
+        volumeNumber++;
+        outFiles.add(dstFileForEPUB3);
+        bwForEPUB3.close();
+      }
+    }
+
+    volumeNumber = 1;
+    if (flagOutputForViewer) {
+      for (ArrayList<String> contentsAsList : volumesAsListForViewer) {
+        fileNameForViewer = "[" + this.author + "] " + this.title + " " + String.format("%02d", volumeNumber) + ".txt";
+        if (!dstPathForViewer.endsWith("/"))
+          dstPathForViewer += "/";
+        dstFileForViewer = new File(dstPathForViewer + fileNameForViewer);
+        fosForViewer = new FileOutputStream(dstFileForViewer);
+        writerForViewer = new OutputStreamWriter(fosForViewer, "UTF-8");
+        bwForViewer = new BufferedWriter(writerForViewer);
+        for (String line : contentsAsList) {
+          bwForViewer.write(line);
+        }
+        volumeNumber++;
+        bwForViewer.close();
+      }
+    }
+
+    return outFiles;
+  }
+
+  public String titlePageForEPUB3(int volumeNumber) {
+    StringBuilder stringBuilder = new StringBuilder();
+    stringBuilder.append(this.title);
+    stringBuilder.append(" ");
+    stringBuilder.append(String.format("%02d", volumeNumber));
+    stringBuilder.append("\n");
+    stringBuilder.append(this.author);
+    stringBuilder.append("\n");
+    if (volumeNumber == 1 && this.summary.length() > 0) {
+      stringBuilder.append("\n［＃区切り線］\n\n［＃ここから２字下げ］\n［＃ここから２字上げ］\n");
+      stringBuilder.append(this.summary);
+      stringBuilder.append("\n［＃ここで字上げ終わり］\n［＃ここで字下げ終わり］\n\n［＃区切り線］\n\n");
+    }
+    stringBuilder.append("［＃改ページ］\n");
+    return stringBuilder.toString();
+  }
+
+  public String titlePageForViewer(int volumeNumber) {
+    StringBuilder stringBuilder = new StringBuilder();
+    stringBuilder.append("［＃ページの左右中央］\n［＃ここから２字下げ］\n［＃ここから大見出し］\n");
+    stringBuilder.append(this.title);
+    stringBuilder.append(" ");
+    stringBuilder.append(String.format("%02d", volumeNumber));
+    stringBuilder.append("\n");
+    stringBuilder.append("［＃ここで字下げ終わり］\n\n");
+    stringBuilder.append("［＃ここから地から２字上げ］\n");
+    stringBuilder.append(this.author);
+    stringBuilder.append("\n");
+    stringBuilder.append("［＃ここで字上げ終わり］\n［＃ここで大見出し終わり］\n");
+    stringBuilder.append("［＃改ページ］\n");
+    if (volumeNumber == 1 && this.summary.length() > 0) {
+      stringBuilder.append("\n［＃区切り線］\n\n［＃ここから２字下げ］\n");
+      stringBuilder.append(this.summary);
+      stringBuilder.append("\n［＃ここで字下げ終わり］\n\n［＃区切り線］\n\n");
+    }
+    stringBuilder.append("［＃改ページ］\n");
+    return stringBuilder.toString();
+  }
+
+  /**
+   * 章のリストをセットする
+   * 
+   * @param listOfChapters
+   */
   public void setListOfChapters(ArrayList<AozoraChapter> listOfChapters) {
     this.listOfChapters = listOfChapters;
   }
@@ -187,6 +432,7 @@ public class AozoraTxt {
     /**
      * @param idx
      *          章番号 0は章分けがない作品で使われる defaultChapter 通常の章は番号1から始まる
+     *          したがって、章分けがある作品では第0章は空であり、章分けがない作品では全ての節が第0章に属する
      * @param title
      *          章タイトル defaultChapterの場合は"defaultChapterTitle"
      */
@@ -199,10 +445,12 @@ public class AozoraTxt {
 
     /**
      * @param idx
-     *          章番号 0は章分けがない作品で使われる defaultChapterに与えられる 通常の章は番号1から始まる
+     *          章番号 0は章分けがない作品で使われる defaultChapter 通常の章は番号1から始まる
+     *          したがって、章分けがある作品では第0章は空であり、章分けがない作品では全ての節が第0章に属する
      * @param title
      *          章タイトル defaultChapterの場合は"defaultChapterTitle"
      * @param listOfSections
+     *          ArrayList<AozoraSection>
      * 
      */
     public AozoraChapter(int idx, String title, ArrayList<AozoraSection> listOfSections) {
@@ -325,6 +573,55 @@ public class AozoraTxt {
 
     public ArrayList<String> getBodyAsList() {
       return this.body;
+    }
+
+    /**
+     * 空行調整をした本文を返す
+     * 
+     * @param allowSingleEmptyLine
+     *          この値がtrueのとき、本文中の単一の空行をそのまま残す パラグラフ毎に空行を挟んでいる作品用
+     *          successiveEmptyLinesLimitと合わせて用いることにより、単一の空行は削除し、連続した空行を一つにまとめることができる
+     * @param successiveEmptyLinesLimit
+     *          連続した空行の最大数 この数を超えて連続する空行はこの数まで減らされる
+     * @return 要素として行を持つArrayList 要素末尾に改行コードは付かない
+     */
+    public ArrayList<String> getEmptyLinesTreatedBody(boolean allowSingleEmptyLine, int successiveEmptyLinesLimit) {
+
+      ArrayList<String> emptyLinesTreatedBody = new ArrayList<>();
+
+      int successiveEmptyLinesCount = 0; // ポインタ直前までの連続した空行の数
+
+      Pattern patternEmptyLine = Pattern.compile("^[　 \t]+$");
+      Matcher matcher;
+
+      for (String line : this.getBodyAsList()) {
+        matcher = patternEmptyLine.matcher(line);
+        if (line.equals("") || matcher.find()) {
+          if (allowSingleEmptyLine) {
+            if (successiveEmptyLinesCount == 0) {
+              emptyLinesTreatedBody.add("");
+            } else if (successiveEmptyLinesCount < successiveEmptyLinesLimit) {
+              emptyLinesTreatedBody.add("");
+            }
+          } else {
+            if (successiveEmptyLinesCount > 0 && successiveEmptyLinesCount < successiveEmptyLinesLimit) {
+              emptyLinesTreatedBody.add("");
+            } else if (successiveEmptyLinesCount == successiveEmptyLinesLimit) {
+              emptyLinesTreatedBody.add("");
+            }
+          }
+          successiveEmptyLinesCount++;
+        } else {
+          if (successiveEmptyLinesCount > 1 && successiveEmptyLinesCount <= successiveEmptyLinesLimit
+              && !allowSingleEmptyLine) {
+            emptyLinesTreatedBody.add(""); // 単一空行を許さない場合で連続空行が許容数以下のとき、最初の空行が削られてるので補完
+          }
+          emptyLinesTreatedBody.add(line); // 文字列を出力
+          successiveEmptyLinesCount = 0;
+        }
+      }
+
+      return emptyLinesTreatedBody;
     }
 
     public String getDatePublished() {
--- nucJ/src/com/github/ghmk5/util/UnicodeString.java
+++ nucJ/src/com/github/ghmk5/util/UnicodeString.java
@@ -6,48 +6,95 @@ package com.github.ghmk5.util;
 public class UnicodeString {
 
   /**
+   * 1バイト文字の一文字あたりの表示桁数を1、多バイト文字の一文字あたりの表示桁数を2とみなした上でunicode文字列の桁揃えを行う
+   * flaggedWidthの正負に従い、1バイトスペースで右ないし左詰めに桁揃えされた文字列を返す
+   * srcStringの表示幅がflaggedWidthの絶対値を超えるとき、flagTruncateがtrueであればflaggedWidthに収まる幅まで切り詰められる
+   * 
    * @param srcString
    *          unicode文字列 ASCIIなら問題ないが、SJISやEUCJだと何が起こるかわからない
    * @param flaggedWidth
    *          揃える桁数 正で右詰め 負で左詰め
+   * @param flagTruncate
+   *          falseであれば切り詰めが行われず、srcStringの表示幅がflaggedWidthの絶対値を超える場合は桁揃えも行われない
    * @return 半角スペースで桁揃えされた文字列
    */
-  public static String format(String srcString, int flaggedWidth) {
+  public static String format(String srcString, int flaggedWidth, boolean flagTruncate) {
     String formattedString = null;
 
-    int width = Math.abs(flaggedWidth);
+    // 収めるべき桁数の絶対値
+    int totalWidth = Math.abs(flaggedWidth);
 
-    int length = 0;
-    StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < srcString.length(); i++) {
-      char c = srcString.charAt(i);
-      // ( 英数字 ) ( \ 記号 ) ( ~ 記号 ) ( 半角カナ )
-      if ((c <= '\u007e') || (c == '\u00a5') || (c == '\u203e') || (c >= '\uff61' && c <= '\uff9f')) {
-        length++;
-      } else { // その他 (全角)
-        length += 2;
-      }
-      if (length <= width) {
-        sb.append(c);
-      } else {
-        break;
+    // flagTruncateがtureであり、かつ文字幅が収めるべき桁数より大きい場合、切り詰める
+    String outString = srcString;
+    if (flagTruncate) {
+      if (UnicodeString.getWidth(srcString) > totalWidth) {
+        outString = UnicodeString.truncate(srcString, totalWidth);
       }
     }
-    String truncatedString = sb.toString();
 
-    sb = new StringBuilder();
-    for (int i = 0; i < (width - length); i++) {
+    // 与えられた桁数を埋める長さの空白を組み立てる
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < totalWidth - (UnicodeString.getWidth(outString)); i++) {
       sb.append(" ");
     }
     String spaces = sb.toString();
 
     if (flaggedWidth > 0) {
-      formattedString = spaces + truncatedString;
+      formattedString = spaces + outString;
     } else {
-      formattedString = truncatedString + spaces;
+      formattedString = outString + spaces;
     }
 
     return formattedString;
+  }
+
+  /**
+   * 全角半角混じりのunicode文字列の幅(半角文字での桁数)を計算する
+   * 
+   * @param string
+   *          幅を知りたい文字列
+   * @return int 半角文字での桁数
+   */
+  public static int getWidth(String string) {
+    int width = 0;
+    for (int i = 0; i < string.length(); i++) {
+      char c = string.charAt(i);
+      // ( 英数字 ) ( \ 記号 ) ( ~ 記号 ) ( 半角カナ )
+      if ((c <= '\u007e') || (c == '\u00a5') || (c == '\u203e') || (c >= '\uff61' && c <= '\uff9f')) {
+        width++;
+      } else { // その他 (全角)
+        width += 2;
+      }
+    }
+    return width;
+  }
+
+  /**
+   * 全角半角混じりのunicode文字列を、半角文字での桁数で与えられる幅まで切り詰める
+   * stringの表示桁数がwidthより小さい場合、stringをそのまま返す
+   * 文字単位で切り詰めると1バイト文字2桁分づつでしか幅を変えられない場合があるため、
+   * stringの表示桁数がwidthより大きい(=切り詰めが行われる)場合でも、戻り値の表示桁数がwidthと等しくならず、1桁短いことがある
+   * 
+   * @param string
+   * @param width
+   * @return
+   */
+  public static String truncate(String string, int width) {
+    String truncatedString;
+    if (UnicodeString.getWidth(string) > width) {
+      StringBuilder sb = new StringBuilder();
+      int j = 0;
+      while (UnicodeString.getWidth(sb.toString()) < width) {
+        sb.append(string.charAt(j));
+        j++;
+      }
+      truncatedString = sb.toString();
+      if (UnicodeString.getWidth(truncatedString) > width)
+        truncatedString = truncatedString.substring(0, truncatedString.length() - 1);
+    } else {
+      truncatedString = string;
+    }
+    return truncatedString;
   }
 
 }
